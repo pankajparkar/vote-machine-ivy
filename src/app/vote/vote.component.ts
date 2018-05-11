d8e8bb7b9e23e0bafc6fda26805a521a81b6e32e
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-05-11 19:46:07 +0530

    Added simple vote component

---
+++ src/app/vote/vote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vm-vote',
+  templateUrl: './vote.component.html',
+  styleUrls: ['./vote.component.css']
+})
+export class VoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
