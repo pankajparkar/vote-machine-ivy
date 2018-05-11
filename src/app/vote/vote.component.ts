b7e7c03a9c7b4b49499220baa6dd25abb0091cc7
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-05-11 19:48:25 +0530

    Added property binding to accept inital votes number

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'vm-vote',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote.component.css']
 })
 export class VoteComponent implements OnInit {
+
+  @Input() votes: number = 0;
 
   constructor() { }
 
