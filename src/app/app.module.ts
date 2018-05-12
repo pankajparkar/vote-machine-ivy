2fb19122a33947c0829345bd5dfa236f73b275ad
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-05-12 09:16:26 +0530

    Removed unwanted files and added gulp task.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector, Type } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
 import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     VoteComponent
   ],
   imports: [
     BrowserModule
   ],
   entryComponents: [VoteComponent],
-  providers: [],
-  // bootstrap: [AppComponent]
 })
 export class AppModule { 
   constructor(private injector: Injector){
